c6fc676d7cca93d3d069aad7f805e42eac748026
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-18 12:38:59 -0300

    Usando react-router-dom (forma recomendada)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/Inicial";
-
-console.log(window.location);
-
-const pagina = window.location.pathname === '/' ? <Inicio/> : <SobreMim/>
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return pagina;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>}/>
+        <Route path="/sobremim" element={<SobreMim/>} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
