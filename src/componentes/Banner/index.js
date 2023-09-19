b7b7881d8d3d980e6e1b90d7bae4da1943058b01
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-19 14:59:25 -0300

    Aplicando estilos no banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,7 +1,33 @@
 import styles from './Banner.module.css';
+import circuloColorido from 'assets/circulo_colorido.png';
+import minhaFoto from 'assets/minha_foto.png'
 
 const Banner = () => {
-    return "Banner";
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+            </div>
+            
+            <p className={styles.paragrafo}>Boas vindas ao meu espaço pessoal! Eu sou Guilherme Serafim, Jovem Aprendiz da Tokio Marine. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :</p>
+            
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidde={true}
+                />
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt="Foto do Guilherme sorrindo"
+                />
+            </div>
+        </div>
+
+    )
 }
 
 export default Banner;
