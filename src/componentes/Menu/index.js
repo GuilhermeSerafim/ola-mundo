99596d66ff9c107d4b380f2c4f29b9437d631787
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-19 13:37:26 -0300

    Criando menu com Link e to do react router

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,6 +1,20 @@
-import './Menu.module.css'
+import { Link } from 'react-router-dom';
+import styles from './Menu.module.css';
+
 const Menu = () => {
-    return "Menu de navegação"
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <Link className={styles.link} to='/'>
+                    Início
+                </Link>
+                <Link className={styles.link} to='/sobremim'>
+                    Sobre Mim
+                </Link>
+            </nav>
+
+        </header>
+    )
 }
 
 export default Menu;
