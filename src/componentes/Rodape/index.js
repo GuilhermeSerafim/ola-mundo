147684b057552950bcbdef29374189f97e14d60b
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-20 14:55:07 -0300

    Utilizando svg como se fosse um componente

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,12 @@
+import styles from './Rodape.module.css';
+//Usar svg como se fosse um componente
+import {ReactComponent as MarcaRegistrada} from 'assets/marca_registrada.svg'
+const Rodape = () => {
+    return (
+        <footer className={styles.rodape}>
+
+        </footer>
+    )
+}
+
+export default Rodape;
