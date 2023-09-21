0a747330067a1d1dfbcb1aa3794595ffa51d3ee5
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-21 12:55:59 -0300

    Criando componente NaoEncontrado - Pagina

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
 import posts from "json/posts.json"
 import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
+import './Post.css'
+import NaoEncontrada from "paginas/NaoEncontrada";
+
 const Post = () => {
     
     //Aqui ele retorna apenas o parametro da url ("1", "2", "3"...)
@@ -10,14 +14,22 @@ const Post = () => {
         return post.id === Number(parametros.id) //Porque o id era uma string e estamos utilizando ===
     })
 
-    console.log(post); //Para entender melhor
+    if(!post) {
+        return (
+            <NaoEncontrada/>
+        )
+    }
 
     return (
         <PostModelo
             fotoCapa={`/assets/posts/${post.id}/capa.png`}
             titulo={post.titulo}
-        >
-
+        >   
+            <div className="post-markdown-container">
+            <ReactMarkdown>
+            {post.texto}
+            </ReactMarkdown>
+            </div>
         </PostModelo>
     )
 }
